8e8c7aee5240f3b5dd7358bd0a5466fc70f1fd74
Sebastian Axelsen <sa@palmtreestatistics.com>
2018-01-14 20:40:53 +0100

    Changes twitter API to get full_text for non-retweets. Adds try-except clause around getting twitter data

--- get_twitter_data.py
+++ get_twitter_data.py
@@ -8,16 +8,30 @@ auth = OAuth1(ss.TWITTER_PUBLIC_CONSUMER_KEY, ss.TWITTER_SECRET_CONSUMER_KEY, ss
 query = '#bitcoin'
 
 tweet_count = 0
-with open('./data/twitter_text.csv', 'w') as csv_file:
-    dict_writer = csv.DictWriter(csv_file, ('created_at', 'user_name', 'followers_count', 'text'))
+with open('./data/twitter_text.csv', 'w', encoding='utf-8') as csv_file:
+    dict_writer = csv.DictWriter(csv_file,
+                                 fieldnames=('created_at', 'user_name', 'followers_count', 'text'),
+                                 quoting=csv.QUOTE_NONNUMERIC)
     dict_writer.writeheader()
-    for tweet_batch in get_recent_tweets(auth, query, max_count=1000, language='en'):
-        for status in tweet_batch['statuses']:
-            dict_writer.writerow({
-                'created_at': status['created_at'],
-                'user_name': status['user']['created_at'],
-                'text': status['text'],
-                'followers_count': status['user']['followers_count']
-            })
-            tweet_count += 1
+
+    tweet_mode = 'extended'
+    followers_limit = 500
+    text_key = 'full_text' if tweet_mode == 'extended' else 'text'
+
+    try:
+        for tweet_batch in get_recent_tweets(auth, query, max_count=1000, language='en', tweet_mode=tweet_mode):
+                for tweet in tweet_batch['statuses']:
+                    user = tweet['user']
+                    if not tweet.get('retweeted_status', None) \
+                            and user.get('followers_count', 0) > followers_limit:
+
+                        dict_writer.writerow({
+                            'created_at': tweet['created_at'],
+                            'user_name': tweet['user']['screen_name'],
+                            'text': tweet[text_key],
+                            'followers_count': user['followers_count']
+                        })
+                        tweet_count += 1
+    except Exception as e:
+        print(e)
 print('Number of tweets: {}'.format(tweet_count))
--- twitter_api/twitter_api.py
+++ twitter_api/twitter_api.py
@@ -1,13 +1,16 @@
 import requests
+from pprint import pprint
 
-BASE_URL = "https://api.twitter.com/1.1/search/tweets.json?"
+BASE_URL = "https://api.twitter.com/1.1/search/tweets.json"
+TWEET_URL = "https://api.twitter.com/1.1/statuses/show.json"
 
 
 def get_recent_tweets_page(auth, query,
                            since_id=None, max_id=None, to_date=None,
-                           count_per_page=100, language='en', result_type='recent'):
-
-    query_params = {'q': query, 'count': count_per_page, 'lang': language, 'result_type': result_type}
+                           count_per_page=100, language='en', result_type='recent',
+                           tweet_mode='compat'):
+    query_params = {'q': query, 'count': count_per_page, 'lang': language, 'result_type': result_type,
+                    'tweet_mode': tweet_mode}
 
     if to_date:
         query_params['until'] = to_date
@@ -21,14 +24,27 @@ def get_recent_tweets_page(auth, query,
     return requests.get(BASE_URL, params=query_params, auth=auth)
 
 
-def get_recent_tweets(auth, query, to_date=None, max_count=1000, language='en', result_type='recent'):
+def get_tweet_by_id(auth, id, tweet_mode='compat'):
+    query_params = {'id': id, 'tweet_mode': tweet_mode}
+
+    return requests.get(TWEET_URL, params=query_params, auth=auth)
+
+
+def get_recent_tweets(auth, query, to_date=None, max_count=1000, language='en', result_type='recent', tweet_mode='compat'):
     n_tweets_processed = 0
     n_tweets_in_batch = 1
+
     while n_tweets_processed < max_count or n_tweets_in_batch == 0:
         if n_tweets_processed == 0:
-            tweet_batch = get_recent_tweets_page(auth, query, to_date=to_date, count_per_page=100, language=language, result_type=result_type).json()
+            tweet_batch = get_recent_tweets_page(auth, query, to_date=to_date, count_per_page=100,
+                                                 language=language, result_type=result_type,
+                                                 tweet_mode=tweet_mode).json()
         else:
-            tweet_batch = get_recent_tweets_page(auth, query, since_id=min_id, max_id=max_id-1, to_date=to_date, count_per_page=100, language=language, result_type=result_type).json()
+            tweet_batch = get_recent_tweets_page(auth, query, since_id=min_id, max_id=max_id - 1, to_date=to_date,
+                                                 count_per_page=100, language=language, result_type=result_type,
+                                                 tweet_mode=tweet_mode).json()
+        if tweet_batch['errors']:
+            raise ValueError('API error: {}'.format(tweet_batch['errors']))
         all_ids = [tweet['id'] for tweet in tweet_batch['statuses']]
         max_id = max(all_ids)
         min_id = min(all_ids)
@@ -37,19 +53,22 @@ def get_recent_tweets(auth, query, to_date=None, max_count=1000, language='en', 
 
         yield tweet_batch
 
+
 if __name__ == '__main__':
     from requests_oauthlib import OAuth1
     import secret_settings as ss
+
     auth = OAuth1(ss.TWITTER_PUBLIC_CONSUMER_KEY, ss.TWITTER_SECRET_CONSUMER_KEY, ss.TWTTER_PUBLIC_ACCESS_TOKEN, ss.TWTTER_SECRET_ACCESS_TOKEN)
 
-    query = '#bitcoin'
-    for tweet_batch in get_recent_tweets(auth, query):
+    query = 'bitcoin'
+    tweet_mode = 'extended'
+    text_key = 'full_text' if tweet_mode == 'extended' else 'text'
+    followers_limit = 500
+
+    for i, tweet_batch in enumerate(get_recent_tweets(auth, query, tweet_mode=tweet_mode)):
         for tweet in tweet_batch['statuses']:
-            print(tweet['text'])
-    # statuses = tweets.json()['statuses']
-    #
-    # print('Number of tweets: {}'.format(len(statuses)))
-    # for status in statuses:
-    #     print(status['user']['name'])
-    #     print(status['user']['followers_count'])
-    #     print(status['created_at'])
+            user = tweet['user']
+            if not tweet.get('retweeted_status', None) \
+                    and user.get('followers_count', 0) > followers_limit:
+                print('{:30} {}'.format(user['screen_name'], tweet[text_key]))
+                print('------------------------')
